d2c0a1f3ee08ab22da23bf21025f14d147f64420
OM <omwadkar003@gmail.com>
2026-05-04 20:05:29 +0530

    New addon.cpp file added

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,24 +1,81 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
-// Load native addon
 const addon = require('../native-addon/build/Release/addon.node');
 
 let mainWindow;
+let isQuitting = false;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
+
+        fullscreen: true,
+        kiosk: true,
+        alwaysOnTop: true,
+
+        minimizable: false,
+        maximizable: false,
+        closable: false,
+
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     });
-    
-    const filePath = path.join(__dirname, 'secure-browser-renderer', 'dist', 'index.html')
+
+    const filePath = path.join(
+        __dirname,
+        'secure-browser-renderer',
+        'dist',
+        'index.html'
+    );
+
     mainWindow.loadFile(filePath);
 
-    // Receive events from C++
+    // ----------------------------
+    // 🔥 WIN-08 / WIN-10: BLOCK CLOSE
+    // ----------------------------
+    mainWindow.on('close', (e) => {
+        if (!isQuitting) {
+            e.preventDefault();
+            mainWindow.webContents.send('security-event', 'CLOSE_BLOCKED');
+        }
+    });
+
+    // ----------------------------
+    // 🔥 WIN-09: BLOCK MINIMIZE
+    // ----------------------------
+    mainWindow.on('minimize', (e) => {
+        e.preventDefault();
+        mainWindow.webContents.send('security-event', 'MINIMIZE_BLOCKED');
+
+        // force restore
+        setTimeout(() => {
+            mainWindow.restore();
+            mainWindow.focus();
+        }, 100);
+    });
+
+    // ----------------------------
+    // 🔥 WIN-11: FOCUS LOSS DETECTION
+    // ----------------------------
+    mainWindow.on('blur', () => {
+        mainWindow.webContents.send('security-event', 'FOCUS_LOST');
+
+        // force focus back
+        setTimeout(() => {
+            mainWindow.focus();
+        }, 100);
+    });
+
+    mainWindow.on('focus', () => {
+        mainWindow.webContents.send('security-event', 'FOCUS_GAINED');
+    });
+
+    // ----------------------------
+    // 🔥 C++ KEY EVENTS
+    // ----------------------------
     addon.onKeyEvent((event) => {
         console.log("From C++:", event);
         mainWindow.webContents.send('key-event', event);
@@ -26,5 +83,13 @@ function createWindow() {
 
     addon.startHook();
 }
+
+// ----------------------------
+// ALLOW SAFE EXIT (ADMIN)
+// ----------------------------
+ipcMain.on('force-exit', () => {
+    isQuitting = true;
+    app.quit();
+});
 
 app.whenReady().then(createWindow);
--- electron-app/preload.js
+++ electron-app/preload.js
@@ -1,9 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-    onKeyEvent: (callback) => {
-        ipcRenderer.on('key-event', (event, data) => {
-            callback(data);
-        });
-    }
+    onKeyEvent: (callback) => ipcRenderer.on('key-event', (_, data) => callback(data)),
+    onSecurityEvent: (callback) => ipcRenderer.on('security-event', (_, data) => callback(data)),
+
+    forceExit: () => ipcRenderer.send('force-exit')
 });
